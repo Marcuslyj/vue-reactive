904414bc1d095476df88e687f569a9f4d7bc5c47
liyongjie <liyongjie@tvflnet.com>
2020-05-13 09:41:37 +0800

    删除多余入口；使用clean-webpack-plugin清理构建目录；添加windows构建命令

--- webpack.build.js
+++ webpack.build.js
@@ -1,15 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const path = require('path')
 
 console.log('path.resolve', path.resolve('./docs'))
 module.exports = {
   mode: 'production',
-  entry: ['./src/index.js', './public/example.js'],
+  entry: [ './public/example.js'], // './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve('./docs')
   },
   plugins: [
+    // 清理构建目录，output路径
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       // 打包输出HTML
       title: 'Reactive',
